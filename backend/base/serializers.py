47c924e1249ea637e6b26556fe54bf2225cef552
roni <ronizk1@gmail.com>
2024-05-30 21:06:18 +0300

    gg

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,9 +1,70 @@
-# tasks/serializers.py
+# # tasks/serializers.py
 
+# from rest_framework import serializers
+# from .models import Task,BigTask
+
+# class TaskSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Task
+#         fields = '__all__'
+
+# class BigTaskSerializer(serializers.ModelSerializer):
+#     subtasks = TaskSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = BigTask
+#         fields = '__all__'
+
+
+# newwwwwwwwwww
 from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    subtasks = serializers.ListField(
+        child=serializers.JSONField(), required=False
+    )
+
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'completed', 'due_date', 'image', 'is_big_task', 'subtasks', 'user']
+        extra_kwargs = {
+            'user': {'read_only': True}
+        }
+
+    def create(self, validated_data):
+        subtasks_data = validated_data.pop('subtasks', [])
+        user = self.context['request'].user
+        task = Task.objects.create(user=user, **validated_data)
+
+        for subtask_data in subtasks_data:
+            subtask = Task.objects.create(user=user, **subtask_data, big_task=task)
+        
+        return task
+
+    def update(self, instance, validated_data):
+        subtasks_data = validated_data.pop('subtasks', [])
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.completed = validated_data.get('completed', instance.completed)
+        instance.due_date = validated_data.get('due_date', instance.due_date)
+        instance.image = validated_data.get('image', instance.image)
+        instance.is_big_task = validated_data.get('is_big_task', instance.is_big_task)
+        instance.save()
+
+        # Clear existing subtasks
+        instance.subtasks.all().delete()
+
+        # Add new subtasks
+        user = self.context['request'].user
+        for subtask_data in subtasks_data:
+            Task.objects.create(user=user, **subtask_data, big_task=instance)
+
+        return instance
+
+class BigTaskSerializer(serializers.ModelSerializer):
+    subtasks = TaskSerializer(many=True)
+
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'completed', 'due_date', 'image', 'is_big_task', 'subtasks', 'user']
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,198 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse, JsonResponse
+# from rest_framework import serializers
+# from .models import Task,BigTask
+# from django.contrib.auth.models import User
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view, permission_classes
+# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+# from rest_framework_simplejwt.views import TokenObtainPairView
+# from rest_framework.permissions import IsAuthenticated
+# from django.shortcuts import get_object_or_404
+# from .serializers import TaskSerializer,BigTaskSerializer
+
+# from rest_framework_simplejwt.views import TokenObtainPairView
+# import logging
+# from rest_framework import status
+# from rest_framework import generics
+
+
+# class TaskSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Task
+#         fields = '__all__'
+        
+# def myTasks(req):
+#     all_tasks = TaskSerializer(Task.objects.all(), many=True).data
+#     return JsonResponse(all_tasks, safe=False)
+
+# # login
+# class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+#     @classmethod
+#     def get_token(cls, user):
+#         token = super().get_token(user)
+#         # Add custom claims
+#         token['username'] = user.username
+#         # ...
+#         return token
+    
+    
+# #     # before
+# # class MyTokenObtainPairView(TokenObtainPairView):
+# #     serializer_class = MyTokenObtainPairSerializer
+
+# logger = logging.getLogger(__name__)
+
+# class MyTokenObtainPairView(TokenObtainPairView):
+#     serializer_class = MyTokenObtainPairSerializer
+
+#     def post(self, request, *args, **kwargs):
+#         response = super().post(request, *args, **kwargs)
+#         if 'access' in response.data:
+#             access_token = response.data['access']
+#             logger.info(f"Access Token: ⭐⭐⭐ {access_token} ⭐⭐⭐")
+#         return response
+
+# # vvvvvvvvvvvvvvvvvv
+
+# # register
+# @api_view(['POST'])
+# def register(request):
+#     user = User.objects.create_user(
+#                 username=request.data['username'],
+#                 email=request.data['email'],
+#                 password=request.data['password']
+#             )
+#     user.is_active = True
+#     user.is_staff = True
+#     user.save()
+#     return Response("new user born")
+
+# # logout
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def logout(request):
+#     # Invalidate the token on the client side
+#     response = Response({"message": "Logged out successfully"}, status=200)
+#     response.delete_cookie('access_token')  # if you are storing JWT in cookies
+#     response.delete_cookie('refresh_token')  # if you are storing refresh token in cookies
+#     return response
+
+# #i’m protected
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def about(req):
+#     return Response("about")
+
+
+
+
+# @api_view(['GET'])
+# def index(req):
+#     return Response('hello')
+
+
+# # קודם
+
+# @api_view(['GET', 'POST', 'DELETE', 'PUT', 'PATCH'])
+# @permission_classes([IsAuthenticated])
+# def tasks(request, id=None):
+#     if request.method == 'GET':
+#         if id is not None:
+#             try:
+#                 task = Task.objects.get(id=id)
+#                 serializer = TaskSerializer(task)
+#                 return Response(serializer.data)
+#             except Task.DoesNotExist:
+#                 return Response("Task not found", status=404)
+#         else:
+#             tasks = Task.objects.all()
+#             serializer = TaskSerializer(tasks, many=True)
+#             return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = TaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+#     elif request.method == 'PUT':
+#         try:
+#             task = Task.objects.get(id=id)
+#         except Task.DoesNotExist:
+#             return Response("Task not found", status=404)
+#         serializer = TaskSerializer(task, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
+#     elif request.method == 'DELETE':
+#         try:
+#             task = Task.objects.get(id=id)
+#         except Task.DoesNotExist:
+#             return Response("Task not found", status=404)
+#         task.delete()
+#         return Response("Task deleted successfully")
+        
+# @api_view(['GET', 'POST', 'DELETE', 'PUT', 'PATCH'])
+# @permission_classes([IsAuthenticated])
+# def big_tasks(request, id=None):
+#     if request.method == 'GET':
+#         if id is not None:
+#             try:
+#                 big_task = BigTask.objects.get(id=id)
+#                 serializer = BigTaskSerializer(big_task)
+#                 return Response(serializer.data)
+#             except BigTask.DoesNotExist:
+#                 return Response("Big task not found", status=404)
+#         else:
+#             big_tasks = BigTask.objects.all()
+#             serializer = BigTaskSerializer(big_tasks, many=True)
+#             return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = BigTaskSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
+#     elif request.method == 'PUT':
+#         try:
+#             big_task = BigTask.objects.get(id=id)
+#         except BigTask.DoesNotExist:
+#             return Response("Big task not found", status=404)
+#         serializer = BigTaskSerializer(big_task, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
+#     elif request.method == 'DELETE':
+#         try:
+#             big_task = BigTask.objects.get(id=id)
+#         except BigTask.DoesNotExist:
+#             return Response("Big task not found", status=404)
+#         big_task.delete()
+#         return Response("Big task deleted successfully")
+    
+    
+#     # checkkkkkkkkkkkkkkkkkkkkkkk
+
+# class TaskListCreateView(generics.ListCreateAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+
+#     def get_serializer_context(self):
+#         context = super().get_serializer_context()
+#         context.update({"request": self.request})
+#         return context
+
+# class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Task.objects.all()
+#     serializer_class = TaskSerializer
+
+#     def get_serializer_context(self):
+#         context = super().get_serializer_context()
+#         context.update({"request": self.request})
+#         return context
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework import serializers
