fb8a0140c49c775469b00adfa43aed0c56702c7b
roni <ronizk1@gmail.com>
2024-06-09 12:05:23 +0300

    gg

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,39 +1,11 @@
-# from django.db import models
 
-# # Create your models here.
-
-# # tasks/models.py
-
-# class BigTask(models.Model):
-#     title = models.CharField(max_length=255)
-
-# class Task(models.Model):
-#     title = models.CharField(max_length=200)
-#     description = models.TextField()
-#     completed = models.BooleanField(default=False)
-#     due_date = models.DateField(null=True, blank=True)
-#     image = models.ImageField(upload_to='task_images/', null=True, blank=True)
-#     big_task = models.ForeignKey(BigTask, related_name='subtasks', on_delete=models.CASCADE, null=True, blank=True)
-    
-
-#     def _str_(self):
-#         return self.title
-
-#     @property
-#     def image_url(self):
-#         if self.image:
-#             return self.image.url
-#         return ''
 
 from django.db import models
 
-# Create your models here.
-
-# tasks/models.py
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     due_date = models.DateField(null=True, blank=True)
     image = models.ImageField(upload_to='task_images/', null=True, blank=True)
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,7 @@
 """
 apppppppppppppppp
 """
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
+
 
 from django.contrib import admin
 from django.urls import path, include
